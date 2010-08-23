f7c009f5a7d0c09d7df53ffaf3c8cf0060831a6e
Robert McGovern <rob@tarasis.net>
2010-08-23 10:34:57 +0200

    Added comment about the updateList method

--- AppController.m
+++ AppController.m
@@ -110,6 +110,7 @@
     //NSLog(@"createList: connectionIdentifier =%@", [twitterEngine createList:@"test1" forUser:TESTING_SECONDARY_USER withPrivacyMode:PRIVATE andDescription:@"A test of list creation"]);
     //NSLog(@"createList: connectionIdentifier =%@", [twitterEngine createList:@"test3" forUser:TESTING_SECONDARY_USER withPrivacyMode:PRIVATE andDescription:nil]);
 	//NSDictionary * testOptions = [NSDictionary dictionaryWithObjectsAndKeys:@"training-hard", @"name", @"my training buddies", @"description", nil]; 
+	// note the listname to update can be either the original list name OR the list ID as a string
 	//NSLog(@"updateList: connectionIdentifier =%@", [twitterEngine updateList:@"19583549" forUser:TESTING_SECONDARY_USER withOptions:testOptions]);
 	
 	//NSLog(@"getListsForUser: connectionIdentifier = %@", [twitterEngine getListsForUser:TESTING_SECONDARY_USER]);
